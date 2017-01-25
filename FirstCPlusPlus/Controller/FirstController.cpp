f764ea506da95d97966c5a9d1234e7b8c9ab6173
Tbone288 <todo3858@740s-pl-117.local>
2017-01-25 13:03:13 -0700

    added specialOutput- added input and output

--- FirstCPlusPlus/Controller/FirstController.cpp
+++ FirstCPlusPlus/Controller/FirstController.cpp
@@ -14,4 +14,27 @@ using namespace std;
 void FirstController :: start()
 {
     cout << "words here please :D" << endl;
+    this ->specialOutput();
+}
+
+void FirstController:: specialOutput()
+{
+    cout << "Hi! How old are you?" << endl;
+    int age;
+    cin >> age;
+    cout << "you typed in " << age << endl;
+    cout << "Type in your name :D" << endl;
+    string name;
+    cin >> name;
+    cout << "your name is " << name << ". Wow. Cool." << endl;
+    cin.ignore();
+    cin >> name;
+    cout << name;
+    cout << "Type your name and age" << endl;
+    cin >> name >> age;
+    cout << "you typed " << name << " and " << age << endl;
+    cout << "Type in your full name" << endl;
+    cin.ignore();
+    getline(cin, name);
+    cout << name << endl;
 }
